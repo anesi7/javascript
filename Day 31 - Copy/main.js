1910eadf953fd01ad3a16338dcc62345bfa587c5
anesi7 <anesvitija7@gmail.com>
2024-12-24 20:46:39 +0100

    loop

---
+++ Day 31 - Copy/main.js
@@ -0,0 +1,39 @@
+
+var i =0;
+do{
+    i= i+1;
+    console.log(i);
+}while(i<5);
+
+console.log("---------------");
+
+
+
+var j = 0;
+while(j<5){
+    j = j+1;
+    console.log(j);
+}
+
+var students =["Bleart","Anes","Elvis"];
+for( var e= 0 ; e<students.length;e++){
+    console.log(students[e]);
+}
+var stu =['hello', "hi", "somthing"]
+
+var y;
+
+for( y of stu){
+    console.log(y);
+}
+
+console.log("---------------");
+
+var teksti = "qa bone ti";
+var o;
+for(o of teksti){
+console.log(o);
+}
+
+var person ={Firstname:"Anes",Surname:"Vitija",Age:15};
+var z=0;
