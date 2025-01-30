b3221b09170f2efffc51a1d6b517a781e9d0c32e
anesi7 <anesvitija7@gmail.com>
2025-01-30 20:48:17 +0100

    Day 35 and part 2

--- Day 35/main.js
+++ Day 35/main.js
@@ -1,17 +1,18 @@
 var slideIndex = 1;
-function showslides(){
-    var i;
-    var slide =document.getElementsByClassName('slide');
 
-for(i=0; i<slide.length; i++){
+function showslides(n){
+    var i;
+    var slides =document.getElementsByClassName('slide');
+
+for(i=0; i<slides.length; i++){
     slides[i].style.display='none';
 }
-if (n> slide.length){
+if (n> slides.length){
     slides = 1;
     
 }
 if (n<1){
-    slide.length = slideIndex;
+    slideIndex = slides.length;
 }
 slides[slideIndex-1].style.display="block";
 }
